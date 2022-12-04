1b58dee0a1682590bc14810250b5d7b31bce27b5
Michael Kohler <me@michaelkohler.info>
2022-12-04 17:37:37 +0100

    Only try to render image if it exists

--- app/components/show-result.tsx
+++ app/components/show-result.tsx
@@ -1,19 +1,16 @@
 import type { Show } from "@prisma/client";
 
-import Spinner from "~/components/spinner";
-import { useTransition, Form } from "@remix-run/react";
+import { Form } from "@remix-run/react";
 
 interface Props {
   show: Show;
 }
 
 export default function ShowResult({ show }: Props) {
-  const transition = useTransition();
-
   return (
     <div className="my-3 flex flex-row rounded-lg border-2 border-slate-300 bg-slate-50 py-5 px-5">
       <div className="flex flex-none flex-col">
-        <img src={show.image_url} alt="" />
+        {show.image_url && <img src={show.image_url} alt="" />}
       </div>
       <div className="flex flex-col">
         <div className={`flex flex-col ${show.image_url ? "pl-10" : ""} pr-10`}>
@@ -44,12 +41,9 @@ export default function ShowResult({ show }: Props) {
             <input type="hidden" name="showId" value={show.maze_id} />
             <button
               type="submit"
-              className="mt-4 rounded bg-slate-600 py-2 px-4 hover:bg-slate-500 active:bg-slate-500"
+              className="mt-4 rounded bg-slate-600 py-2 px-4 text-white hover:bg-slate-500 active:bg-slate-500"
             >
-              {!transition.submission && (
-                <span className="text-white">Add Show</span>
-              )}
-              {!!transition.submission && <Spinner />}
+              Add Show
             </button>
           </Form>
         </div>
