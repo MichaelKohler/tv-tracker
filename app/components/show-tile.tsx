333ea1ff16dfe3bdf861cf736aa5784d799dd9e0
Michael Kohler <me@michaelkohler.info>
2022-12-19 00:19:59 +0100

    Add spinner on tile when navigating to detail page (fixes #5)

--- app/components/show-tile.tsx
+++ app/components/show-tile.tsx
@@ -1,11 +1,17 @@
-import { Link } from "@remix-run/react";
+import { Link, useTransition } from "@remix-run/react";
 
+import Spinner from "./spinner";
 import type { FrontendShow } from "~/utils";
 interface Props {
   show: FrontendShow;
 }
 
 export default function ShowTile({ show }: Props) {
+  const transition = useTransition();
+  const navigatingToDetail =
+    transition.state === "loading" &&
+    transition.location.pathname === `/tv/${show.id}`;
+
   return (
     <Link to={`/tv/${show.id}`}>
       <div className="relative my-3 mr-3 flex min-w-[250px] max-w-[250px] flex-col rounded-lg border-2 border-slate-100 hover:bg-slate-100">
@@ -17,6 +23,11 @@ export default function ShowTile({ show }: Props) {
               !show.unwatchedEpisodesCount ? "grayscale-80" : ""
             }`}
           />
+        )}
+        {navigatingToDetail && (
+          <div className="absolute top-32 left-[103px] mt-4">
+            <Spinner />
+          </div>
         )}
         {(show.unwatchedEpisodesCount ?? 0) > 0 && (
           <div className="absolute top-0 left-0 rounded-tl-lg bg-orange-400 py-2 px-2 text-xl">
