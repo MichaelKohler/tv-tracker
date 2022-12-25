276a19946a7c0f4b695a40c2d15d613791292385
Michael Kohler <me@michaelkohler.info>
2022-12-25 13:11:44 +0100

    Move all account related E2E tests into same file

--- cypress/e2e/account.ts
+++ cypress/e2e/account.ts
@@ -1,5 +1,39 @@
 import { faker } from "@faker-js/faker";
 
+describe("login tests", () => {
+  afterEach(() => {
+    cy.cleanupUser();
+  });
+
+  it("should allow you to register and login", () => {
+    const loginForm = {
+      email: `${faker.internet.userName()}@example.com`,
+      password: faker.internet.password(),
+      username: faker.hacker.noun(),
+    };
+    cy.then(() => ({ email: loginForm.email })).as("user");
+
+    cy.visitAndCheck("/");
+
+    cy.findByRole("link", { name: /sign up/i }).click();
+    cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
+    cy.findByLabelText(/password/i).type(loginForm.password);
+    cy.findByRole("button", { name: /create account/i }).click();
+
+    cy.findByRole("link", { name: /TV/ }).click();
+    cy.findByText(/Your Shows/i);
+
+    cy.findByRole("button", { name: /logout/i }).click();
+
+    cy.findByRole("link", { name: /log in/i }).click();
+    cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
+    cy.findByLabelText(/password/i).type(loginForm.password);
+    cy.findByRole("button", { name: /log in/i }).click();
+    cy.findByRole("link", { name: /TV/ }).click();
+    cy.findByText(/Your Shows/i);
+  });
+});
+
 describe("account deletion tests", () => {
   it("should allow to delete account with confirmation", () => {
     cy.login();
@@ -26,5 +60,60 @@ describe("account deletion tests", () => {
     cy.findByRole("textbox", { name: /email/i }).type(loginForm.email);
     cy.findByLabelText(/password/i).type(loginForm.password);
     cy.findByRole("button", { name: /create account/i }).click();
+  });
+});
+
+describe("password tests", () => {
+  const email = "foo@example.com";
+  const newPassword = faker.internet.password();
+
+  afterEach(() => {
+    cy.cleanupUser();
+  });
+
+  it("should allow to change password", () => {
+    cy.login({ email });
+    cy.visitAndCheck("/");
+
+    cy.findByRole("link", { name: /account/i }).click();
+    cy.findByRole("link", { name: /change password/i }).click();
+
+    cy.findByLabelText(/current password/i).type("myreallystrongpassword");
+    cy.findByLabelText(/new password/i).type(newPassword);
+    cy.findByLabelText(/confirm password/i).type(newPassword);
+    cy.findByRole("button", { name: /change password/i }).click();
+
+    cy.findByRole("button", { name: /logout/i }).click();
+
+    cy.findByRole("link", { name: /log in/i }).click();
+    cy.findByRole("textbox", { name: /email/i }).type(email);
+    cy.findByLabelText(/password/i).type(newPassword);
+    cy.findByRole("button", { name: /log in/i }).click();
+    cy.findByRole("link", { name: /TV/ }).click();
+    cy.findByText(/Your Shows/i);
+  });
+
+  it("should check for non-matching passwords", () => {
+    cy.login({ email });
+    cy.visitAndCheck("/");
+
+    cy.findByRole("link", { name: /account/i }).click();
+    cy.findByRole("link", { name: /change password/i }).click();
+
+    cy.findByLabelText(/current password/i).type("myreallystrongpassword");
+    cy.findByLabelText(/new password/i).type(newPassword);
+    cy.findByLabelText(/confirm password/i).type("foo");
+    cy.findByRole("button", { name: /change password/i }).click();
+
+    cy.findByLabelText(/new password/i).type(newPassword);
+    cy.findByLabelText(/confirm password/i).type(newPassword);
+    cy.findByRole("button", { name: /change password/i }).click();
+  });
+});
+
+describe("password tests - logged out", () => {
+  it("should ask for login before showing change password form", () => {
+    cy.visitAndCheck("/password/change");
+    cy.findByRole("link", { name: /log in/i });
   });
 });
