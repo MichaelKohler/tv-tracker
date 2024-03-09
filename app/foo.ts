f808c2f290d1501f27b863b464445861193c950c
Michael Kohler <me@michaelkohler.info>
2024-03-09 19:13:16 +0100

    Add OpenTelemetry instrumentation in entry.server file

---
+++ app/foo.ts
@@ -0,0 +1,15 @@
+import { init } from "./instrumentation";
+init();
+
+import { createRequestHandler } from "@vercel/remix/server";
+import * as build from "@remix-run/dev/server-build";
+
+export default createRequestHandler({
+  build,
+  mode: process.env.NODE_ENV,
+  getLoadContext() {
+    return {
+      nodeLoadContext: true,
+    };
+  },
+});
