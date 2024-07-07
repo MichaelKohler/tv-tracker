d1499b06b79737fda762204769646fccceda6be2
Michael Kohler <me@michaelkohler.info>
2024-07-07 11:39:12 +0200

    Add logs to prisma client init

--- app/db.server.ts
+++ app/db.server.ts
@@ -5,9 +5,14 @@ import { createClient } from "@libsql/client";
 let prisma: PrismaClient;
 const forceTurso = process.env.TURSO_FORCE === "true";
 
+console.log("Initializing prisma client");
 if (process.env.NODE_ENV !== "production" && !forceTurso) {
+  console.log("Falling back to local prisma client");
   prisma = new PrismaClient();
 } else {
+  console.log("Initializing libsql client", {
+    url: process.env.TURSO_DATABASE_URL,
+  });
   const libsql = createClient({
     url: `${process.env.TURSO_DATABASE_URL}`,
     authToken: `${process.env.TURSO_AUTH_TOKEN}`,
