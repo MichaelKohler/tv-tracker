a18c9eaaa24963c34943591ff8f5c5f3e48ba4c1
Michael Kohler <me@michaelkohler.info>
2024-08-15 18:05:29 +0200

    Use web version of libsql

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaLibSQL } from "@prisma/adapter-libsql";
-import { createClient } from "@libsql/client";
+import { createClient } from "@libsql/client/web";
 
 let prisma: PrismaClient;
 const forceTurso = process.env.TURSO_FORCE === "true";
