10da6f57c919fb2d99e2df6238d9ed3bba98e15b
Michael Kohler <me@michaelkohler.info>
2024-12-17 19:51:58 +0100

    Add log for fetched episode in update script

--- scripts/force-episode-update.ts
+++ scripts/force-episode-update.ts
@@ -41,6 +41,8 @@ async function updateEpisode(
       }
     );
 
+    console.log("Fetched episode", fetchedEpisode);
+
     const newRecord = {
       mazeId: `${fetchedEpisode.id}`,
       name: fetchedEpisode.name,
