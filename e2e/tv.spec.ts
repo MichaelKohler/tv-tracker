9c6559f9d5dfca012fc92156e95dccd10193e8de
Michael Kohler <me@michaelkohler.info>
2025-11-12 23:19:21 +0100

    Fix E2E test

--- e2e/tv.spec.ts
+++ e2e/tv.spec.ts
@@ -64,7 +64,7 @@ test("allows TV flows", async ({ page }) => {
   await expect(page.getByText("Mark as not watched").nth(0)).toBeVisible();
   await page.getByText("Mark as not watched").nth(0).click();
 
-  await expect(page.getByText("Ignore unwatched on overview")).toBeVisible();
+  await expect(page.getByText("Archive")).toBeVisible();
 
   await page.getByText("Remove show").click();
   await expect(
