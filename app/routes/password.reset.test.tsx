cda769540af6c9a4ad639717c3b62c0846e85493
Michael Kohler <me@michaelkohler.info>
2025-11-13 12:21:32 +0100

    Remove unused import

--- app/routes/password.reset.test.tsx
+++ app/routes/password.reset.test.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { redirect, useActionData } from "react-router";
+import { useActionData } from "react-router";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
