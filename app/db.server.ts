9de07540f7d0cfa919e1e60f64345de6df6eba54
Michael Kohler <me@michaelkohler.info>
2025-12-24 16:30:53 +0100

    Prepare Prisma upgrade to version 7

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client.js";
 
 let prisma: PrismaClient;
 
@@ -6,15 +7,19 @@ declare global {
   var __db__: PrismaClient;
 }
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
 // this is needed because in development we don't want to restart
 // the server with every change, but we want to make sure we don't
 // create a new connection to the DB with every change either.
 // in production we'll have a single connection to the DB.
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient({ adapter });
 } else {
   if (!global.__db__) {
-    global.__db__ = new PrismaClient();
+    global.__db__ = new PrismaClient({ adapter });
   }
   prisma = global.__db__;
   prisma.$connect();
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 import bcrypt from "bcrypt";
 
-const prisma = new PrismaClient();
+import { PrismaClient } from "../app/generated/prisma/client";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+const prisma = new PrismaClient({ adapter });
 
 async function seed() {
   const email = "rachel@remix.run";
