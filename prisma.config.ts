0b1be1367a0d2265c06ee2ff939d34d1b5bc576b
Michael Kohler <me@michaelkohler.info>
2025-12-24 16:30:53 +0100

    Prepare Prisma upgrade to version 7

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from "prisma/config";
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
   migrations: {
-    seed: "node --experimental-strip-types prisma/seed.ts",
+    seed: "tsx --env-file .env prisma/seed.ts",
   },
 });
