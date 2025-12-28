934e0ff1a02de53787137fb8270268863fb3c619
Michael Kohler <me@michaelkohler.info>
2025-12-28 19:40:03 +0100

    Use alias to make Prisma work again

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,20 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [!process.env.VITEST && reactRouter()],
 
+  resolve: {
+    alias: {
+      "@prisma/client": "./app/generated/prisma/index.js",
+    },
+  },
+
+  ssr: {
+    noExternal: ["@prisma/client"],
+  },
+
   build: {
     sourcemap: true,
+    commonjsOptions: {
+      include: [/app\/generated\/prisma/, /node_modules/],
+    },
   },
 });
