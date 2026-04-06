1c8a22362d0c5a9f0cef225e3292219f8687f2b4
Michael Kohler <me@michaelkohler.info>
2026-04-06 07:41:46 +0000

    Add a minimum maxAge in case session is longer (#309)

--- app/session.server.ts
+++ app/session.server.ts
@@ -93,7 +93,7 @@ export async function createUserSession({
       "Set-Cookie": await sessionStorage.commitSession(session, {
         maxAge: remember
           ? 60 * 60 * 24 * 7 // 7 days
-          : undefined,
+          : 60 * 60 * 24, // 24 hours
       }),
     },
   });
